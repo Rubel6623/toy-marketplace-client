f92b814011fb506ad6c178321de0d1491c01cf5a
Rubel Rudra <rubelrudra27@gmail.com>
2023-05-22 01:36:52 +0600

    Implement Simple Navbar

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import Footer from '../Pages/Shared/Footer';
+import Footer from '../Pages/Shared/Footer/Footer';
+import Header from '../Pages/Shared/Navbar/Header';
 
 const Main = () => {
     return (
         <div>
+            <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
