9c47507c07fa7602fd5268c2a31b9e270f9e4d57
Rubel Rudra <rubelrudra27@gmail.com>
2023-05-23 15:28:24 +0600

    Complete Login Sign Up with firebase

--- src/Pages/Login/SignUp.jsx
+++ src/Pages/Login/SignUp.jsx
@@ -1,9 +1,32 @@
 import { Link } from "react-router-dom";
 import img from '../../assets/login/login.svg';
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 
 const SignUp = () => {
-    return (
+
+  const {createUser}=useContext(AuthContext);
+
+  const handleSignIn=(event)=>{
+    event.preventDefault();
+
+    const form=event.target;
+    const name=form.name.value;
+    const email=form.email.value;
+    const password=form.password.value;
+    const photo=form.photo.value;
+    console.log(name,email, password,photo)
+
+    createUser(email,password)
+    .then(result=>{
+      const user=result.user;
+      console.log(user);
+    })
+    .catch(error=>console.log(error))
+  } 
+
+  return (
 <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
 
@@ -14,6 +37,8 @@ const SignUp = () => {
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
       <div className="card-body">
         <h2 className="text-4xl font-bold text-center">Sign Up !</h2>
+
+        <form onSubmit={handleSignIn}>
 
         <div className="form-control">
           <label className="label">
@@ -41,16 +66,18 @@ const SignUp = () => {
             <span className="label-text">Photo URL</span>
           </label>
           <input type="text" name="photo" placeholder="photo url" className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
         </div>
+          <div className="form-control mt-12">
+                  <input
+                    className="btn btn-primary"
+                    type="submit"
+                    value="Sign Up"
+                  />
+          </div>
+        </form>
 
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
           <p className='text-center my-3'>Already Have an Account? <Link to='/login' className='text-orange-600 font-semibold'>Please Login</Link></p>
 
-        </div>
       </div>
     </div>
   </div>
