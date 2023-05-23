25e4a859da768c41b75a2f644506327d6ebf266b
Rubel Rudra <rubelrudra27@gmail.com>
2023-05-23 13:37:11 +0600

    Connect to firebase and create a env file

--- src/Pages/HomePage/ShopByCategory/ShopByCategory.jsx
+++ src/Pages/HomePage/ShopByCategory/ShopByCategory.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import ToysCard from './ToysCard';
 
 const ShopByCategory = () => {
+  const [toys,setToys]=useState([]);
+
+  useEffect(()=>{
+    fetch('toys.json')
+    .then(res=>res.json())
+    .then(data=>setToys(data))
+  },[])
+
     return (
         <div className='text-center'>
             <h2 className='text-4xl font-semibold text-center mb-6'>Shop by Category</h2>
@@ -27,24 +37,18 @@ const ShopByCategory = () => {
             </button>
           </Tab>
 
-          <Tab className={' bg-[#ff6799] rounded-3xl '}>
-            <button className='btn btn-outline rounded-3xl px-12'>
-            engineering tools
-            </button>
-          </Tab>
-
         </TabList>
 
         <TabPanel>
-          <h2>Any content 1</h2>
+          <div className='grid md:grid-cols-3 gap-6'>
+          {
+            toys.map(toy=><ToysCard key={toy.id} toy={toy}></ToysCard>)
+          }
+          </div>
         </TabPanel>
 
         <TabPanel>
           <h2>Any content 2</h2>
-        </TabPanel>
-
-        <TabPanel>
-          <h2>Any content 3</h2>
         </TabPanel>
 
         <TabPanel>
---
+++ src/Pages/HomePage/ShopByCategory/ToysCard.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+
+
+const ToysCard = ({toy}) => {
+    const {toyName,toyImg,price,rating}=toy;
+    return (
+        <div className="card w-96 glass shadow-2xl">
+          <figure>
+            <img
+              className="w-"
+              src={toyImg}
+              alt="car!"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{toyName}</h2>
+            <p>Prices : {price}</p>
+            <p>Ratings : {rating}</p>
+            <Link>
+          <button className="btn btn-outline btn-info btn-block">View Details</button>
+          </Link>
+          </div>
+        </div>
+    );
+};
+
+export default ToysCard;
